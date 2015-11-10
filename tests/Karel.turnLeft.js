1c4b498bf42dc95bf9b5c654ce32c1f85d52c376
joymon <joymon@gmail.com>
2015-11-10 11:39:16 -0500

    Added turnLeft test

---
+++ tests/Karel.turnLeft.js
@@ -0,0 +1,21 @@
+﻿describe('Karel', function () {
+
+    it('sanity check', function () {
+        expect(0).toBe(0);
+    });
+    it('turnLeft - From North to West', function () {
+        var karel = new Karel();
+        karel.turnLeft();
+        expect(karel.direction).toBe(Direction.West);
+        console.log("Completed test:turnLeft - From North to West");
+    });
+
+    it('turnLeft - 3 turnLefts should be equal to turnRight', function () {
+        var karel = new Karel();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        expect(karel.direction).toBe(Direction.East);
+        console.log('Completed test:turnLeft - 3 turnLefts should be equal to turnRight');
+    });
+});
